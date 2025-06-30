63aac579d3d0565593ccc6ff6286f9a3a04843f9
Josue Adrian Sosa Cruz <jasosac@unjbg.edu.pe>
2025-06-30 04:41:34 -0500

    Funciones: buscar y listar producto

--- Ventras_productos.cpp
+++ Ventras_productos.cpp
@@ -26,7 +26,7 @@ int contadorVentas = 1;
 
 void registrarProducto() {
     if (numProductos >= MaxiProductos) {
-        cout << "No se pueden agregar más productos.\n";
+        cout << "No se pueden agregar mas productos.\n";
         return;
     }
     cin.ignore();
@@ -36,4 +36,33 @@ void registrarProducto() {
     cin >> productos[numProductos].precio;
     numProductos++;
     cout << "Producto registrado.\n";
+}
+
+void listarProductos() {
+    if (numProductos == 0) {
+        cout << "No hay productos registrados.\n";
+        return;
+    }
+    cout << "\nLISTADO DE PRODUCTOS:\n";
+    for (int i = 0; i < numProductos; i++) {
+        cout << i + 1 << ". " << productos[i].nombre << " | Precio: $" << productos[i].precio << endl;
+    }
+}
+
+void buscarProducto() {
+    cin.ignore();
+    string nombre;
+    cout << "Nombre del producto a buscar: ";
+    getline(cin, nombre);
+    bool encontrado = false;
+    for (int i = 0; i < numProductos; i++) {
+        if (productos[i].nombre == nombre) {
+            cout << "Producto encontrado: " << productos[i].nombre << " | Precio: $" << productos[i].precio << endl;
+            encontrado = true;
+            break;
+        }
+    }
+    if (!encontrado) {
+        cout << "Producto no encontrado.\n";
+    }
 }
